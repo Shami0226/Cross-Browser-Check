e971daa3385b648e9264b79dc31bc68f15aa7f4c
Shami0226 <75041811+Shami0226@users.noreply.github.com>
2021-07-08 23:02:42 +0530

    Update CrossBrowserChecking.java

--- CrossBrowserChecking.java
+++ CrossBrowserChecking.java
@@ -18,16 +18,16 @@ public class CrossBrowserChecking {
 	public void setUpBrowser(String browserName)
 	{
 		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webDriver.chrome.driver",System.getProperty("user.dir")+"Drivers//chromedriver.exe");
+		if(browserName.equalsIgnoreCase("chrome")) {										// Check if parameter passed as chrome
+			System.setProperty("webDriver.chrome.driver",System.getProperty("user.dir")+"Drivers//chromedriver.exe"); 	// Set path to the chromedriver.exe
 			driver=new ChromeDriver();
 			
-		}else if(browserName.equalsIgnoreCase("firefox")){
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"Drivers//geckodriver.exe");
+		}else if(browserName.equalsIgnoreCase("firefox")){									// Check if parameter passed as firefox
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"Drivers//geckodriver.exe");	// Set path to the geckodriver.exe
 			driver=new FirefoxDriver();
 			
-		}else if(browserName.equalsIgnoreCase("ie")){
-			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"Drivers//iedriver.exe");
+		}else if(browserName.equalsIgnoreCase("ie")){										// Check if parameter passed as IE
+			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"Drivers//iedriver.exe"); 		// Set path to the iedriver.exe
 			driver=new InternetExplorerDriver();
 		}else {
 			
@@ -39,18 +39,18 @@ public class CrossBrowserChecking {
 	@Test
 	public void login()
 	{
-		driver.get("https://www.amazon.com/");
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.com/");						// Navigate to the amazon.com website
+		driver.manage().window().maximize();						// Maximize the window size
+		driver.manage().deleteAllCookies();						// Deleting all the cookies
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);		// Set the page load time out
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);		// Set implicit wait 
 		
 		
 	}
 	
 	public void tearDown() 
 	{
-		driver.close();
+		driver.close();									// Close the window
 	}
 
 }
